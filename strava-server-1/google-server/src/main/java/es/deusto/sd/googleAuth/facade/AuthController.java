f43a16fffe27d7288d91379476e4d0ce3a800f67
Javier Gomez <gomez.javier@opendeusto.com>
2024-11-25 18:51:26 +0100

    Google server working

--- strava-server-1/google-server/src/main/java/es/deusto/sd/googleAuth/facade/AuthController.java
+++ strava-server-1/google-server/src/main/java/es/deusto/sd/googleAuth/facade/AuthController.java
@@ -1,5 +1,29 @@
 package es.deusto.sd.googleAuth.facade;
 
-public class AuthController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import es.deusto.sd.googleAuth.service.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+	private AuthService authService;
+	
+	public AuthController(AuthService authService) {
+		this.authService = authService;
+	}
+	
+	@GetMapping("/verify")
+	public ResponseEntity<Boolean> verify(@RequestParam("email") String email, @RequestParam("password") String password){
+		if(authService.verify(email, password)) {
+			return new ResponseEntity<Boolean>(true,HttpStatus.OK);
+		}
+		return new ResponseEntity<Boolean>(false,HttpStatus.FORBIDDEN);
+	}
+	
 }
