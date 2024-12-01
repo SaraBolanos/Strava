44c8cc2919e2c166ada9f76c9bc833b146f08071
jonmendizabal <jonmendizabal@localhost>
2024-12-01 16:38:58 +0100

    NativeSQL in WorkoutRepo

--- strava-server-1/strava-server-1/src/main/java/es/deusto/sd/strava/dao/WorkoutRepository.java
+++ strava-server-1/strava-server-1/src/main/java/es/deusto/sd/strava/dao/WorkoutRepository.java
@@ -15,12 +15,14 @@ import java.util.Optional;
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
 
     // Find workouts by creator (user who created the workout)
-    @Query("SELECT w FROM Workout w WHERE w.user = :user")
+    @Query(value = "SELECT * FROM Workout w WHERE w.user = :user",nativeQuery = true)
     List<Workout> findByUser(@Param("user") User user);
     
     
 
     // Find workout by id
-    @Query("SELECT w FROM Workout w WHERE w.id = :id")
+    @Query(value = "SELECT * FROM Workout w WHERE w.id = :id", nativeQuery = true)
     Optional<Workout> findById(@Param("id") long id);
+    
+    
 }
